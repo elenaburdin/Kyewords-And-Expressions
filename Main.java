e84edf8705b73cabca8922c744d99dd716b38b1b
elenaburdin <66010914+elenaburdin@users.noreply.github.com>
2022-08-17 21:42:31 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package academy.learningprogramming;
+
+public class Main {
+    public static void main(String[] args) {
+        double kilometres = (100 * 1.609344);
+
+        int highScore = 50;
+        if (highScore == 50) {
+            System.out.println("This is an expression");
+        }
+
+    }
+}
